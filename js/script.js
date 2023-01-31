25899063a5f53493856101b387b5aac8aab31610
Levandovskiy <levandovskiylavini@gmail.com>
2023-02-01 00:02:21 +0200

    add to render

--- js/script.js
+++ js/script.js
@@ -141,7 +141,7 @@ setClock('.timer', deadLine);
             }
         });
 
-    const modalTimerId = setTimeout(opnModal,3000);
+   // const modalTimerId = setTimeout(opnModal,3000);
 
     function showModalByScroll() {
         if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight -1) {
@@ -153,5 +153,48 @@ setClock('.timer', deadLine);
 
     window.addEventListener('scroll', showModalByScroll );
     
-  
+    // Використовуємо класи для карточок!
+
+
+    class menuCard {
+        constructor(src, alt, title, descr, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.tranfer = 37;
+            this.chenngeToUAH();
+
+        }
+        chenngeToUAH() {
+            this.price = this.price * this.tranfer;
+        }
+
+        render() {
+            const element = document.createElement('div');
+            element.innerHTML = `
+                    <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>
+            `;
+            this.parent.append(element);
+        }
+    }
+  new menuCard(
+    "img/tabs/vegy.jpg",
+    "vegy",
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    10,
+    '.menu .container'
+  ).render();
 });
